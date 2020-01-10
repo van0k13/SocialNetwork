bbaa57207adfd722019b179e7979bf5df1cc62ba
moks <moks@W10UAHQ01097.awah-ua.ua.auchan.com>
2020-01-10 19:31:38 +0200

    test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,6 +34,9 @@ const Navbar = () => {
                     <NavLink to='/dialogs' activeClassName={styles.activeLink}>Messages</NavLink>
                 </div>
                 <div className={styles.item}>
+                    <NavLink to='/users' activeClassName={styles.activeLink}>Users to Find</NavLink>
+                </div>   
+                <div className={styles.item}>
                     <NavLink to='/news' activeClassName={styles.activeLink}>News</NavLink>
                 </div>
                 <div className={styles.item}>
@@ -42,9 +45,7 @@ const Navbar = () => {
                 <div className={styles.item}>
                     <NavLink to='/settings' activeClassName={styles.activeLink}>Settings</NavLink>
                 </div>
-                <div className={styles.item}>
-                    <NavLink to='/users' activeClassName={styles.activeLink}>Users to Find</NavLink>
-                </div>
+                
 
             </nav>
             <div className={styles.friendsBar}>
